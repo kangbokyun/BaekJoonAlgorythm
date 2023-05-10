6190dc6d6cf85c93c8a39638f9b11f493533e99b
kangbokyun <bk940204@naver.com>
2023-05-10 14:51:35 +0900

    Algorithm_Day7_DoubleArray

---
+++ src/main/java/Algorythm/Day7/DoubleArray_01_2738.java
@@ -0,0 +1,4 @@
+package Algorythm.Day7;
+
+public class DoubleArray_01_2738 {
+}
--- src/main/java/Algorythm/Main.java
+++ src/main/java/Algorythm/Main.java
@@ -1,11 +1,11 @@
 package Algorythm;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		//
+		Scanner sc = new Scanner(System.in);
 	}
 }
